0700ffb099762d6f925d8ea38c3dfcfc802ee043
人间烟火 <772144197@qq.com>
2025-03-21 20:33:15 +0800

    添加跨域

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
-import express, { Express } from "express";
+import express from "express";
+import cors from "cors";
 import "express-async-errors";
 import path from "path";
 import apiRoute from "./routes/api";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
